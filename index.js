341bd1a7de05d56c67054781c91111a8316ef081
pavithra <pavithra.mendon@juegostudio.net>
2021-02-09 11:37:09 +0530

    mocha to jest

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 //importing node framework
 var express = require('express');
- 
+const bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
- res.send('hello world');
+app.get('/', (req, res) => {
+  // var user = {user_id:1, user_name:'abc'}
+  res.send({
+    responseCode:200,
+    responseMessage:'Everything worked as expected',
+    responseData: {}
+  });
 });
+
+app.post('/', (req,res)=>{
+
+  const userId = Number(req.body.userId);
+  res.send({
+    responseCode:200,
+    responseMessage:'Everything worked as expected',
+    responseData: {user_id:userId}
+  });
+})
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
  
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,25 @@
 var request = require('supertest');
 var app = require('../index.js');
+  
+var request = require('supertest');
+var app = require('../index.js');
+
 describe('GET /', function() {
- it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
- });
+  it('test response', (done)=>{
+    request(app).get('/').expect({
+        responseCode:200,
+        responseMessage:'Everything worked as expected',
+        responseData: {}
+    },done);
+  });
+});
+
+describe('POST /', function() {
+  it('test response', (done)=>{
+    request(app).post('/').send({'userId':1}).expect({
+        responseCode:200,
+        responseMessage:'Everything worked as expected',
+        responseData: {user_id:1}
+    },done);
+  });
 });
